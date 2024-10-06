8b418b96666bd92f74c7cdfdbe2ef91fef6a6447
alexan <alexandre>
2024-10-06 15:17:07 -0700

    Initial Commit

---
+++ Split itTests/Split_itTests.swift
@@ -0,0 +1,17 @@
+//
+//  Split_itTests.swift
+//  Split itTests
+//
+//  Created by Alexandre Marque on 10/6/24.
+//
+
+import Testing
+@testable import Split_it
+
+struct Split_itTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
